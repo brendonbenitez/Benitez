d48be04ec49b72fd3bfda7e72d0ecf3ffcb5e12e
brendonbenitez <benitezbrendon4@gmail.com>
2025-08-13 14:32:32 +0800

    airfare

---
+++ src/AIrFAre/airfare.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AirFare;
+
+import java.util.Scanner;
+
+public class AirFARE  {
+
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    
+    int distance;
+    int cl = 0;
+    float total = 0;
+    float discount = 0;
+    
+    System.out.println("Distance (km): ");
+    distance = sc.nextInt();
+    
+    System.out.println("Class: ");
+    cl = sc.nextInt();
+    
+     if (cl == 1){
+            total = distance * 250;
+            if (distance >= 1000){
+                discount = total * (float)0.10;
+                total = total - distance;
+            }
+            System.out.printf("Total Fare :%.2f ", total);
+            
+     }else if (cl == 2){
+         total = distance * 500;
+         if (distance >= 1000){
+             discount = total *(float)0.10;
+             total = total -distance;
+         }
+         System.out.printf("Total Fare :%.2f ", total);
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+   
+  
